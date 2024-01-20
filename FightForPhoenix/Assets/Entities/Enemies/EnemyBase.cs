c90cd9712a2dde579705b2c73a8e3ef2a8fb2f0f
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-20 13:19:54 -0600

    trail renderer to player prefab.

--- FightForPhoenix/Assets/Entities/Enemies/EnemyBase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EnemyBase", menuName = "FightForPhoenix/EnemyBase", order = 0)]
-public class EnemyBase : ScriptableObject {
-    //entity attempts to dodge or not
-    
-}
-
