8a77c2c48b6b6b8d70ea9e30c604e8ac1d345a18
Kamau Vassall <kamau.vassall@gmail.com>
2024-01-19 23:16:35 -0600

    Addign fake tiles in

--- FightForPhoenix/Assets/Scripts/Bullet.cs
+++ FightForPhoenix/Assets/Scripts/Bullet.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class Bullet : MonoBehaviour 
+public class Bullet : MonoBehaviour
 {
     // This will break once there are multple tile maps
     Tilemap tilemap;
@@ -9,7 +9,6 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         tilemap = FindObjectOfType<Tilemap>();
-        gameObject.AddComponent<Rigidbody2D>();
         Debug.Assert(tilemap != null, "You forget to add a Tiilemap to the scene");
     }
 
@@ -28,14 +27,37 @@ public class Bullet : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (tilemap.gameObject == collision.gameObject)
-        {
-            Vector3 hitPosition = Vector3.zero;
-            foreach (ContactPoint2D hit in collision.contacts)
-            {
-                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
-                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
-                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+        if (tilemap.gameObject == collision.gameObject) {
+            Debug.Log("Hit tile");
+            foreach (var hit in collision.contacts) {
+                Vector3 hitWorldPosition = Vector3.zero;
+                hitWorldPosition.x = hit.point.x + 0.01f * hit.normal.x;
+                hitWorldPosition.y = hit.point.y + 0.01f * hit.normal.y;
+                Debug.Log($"hit {hitWorldPosition}");
+
+                Vector3Int cellPosition = tilemap.WorldToCell(hitWorldPosition - new Vector3Int(0, 1, 0));
+                Debug.Log($"cell {cellPosition}");
+
+                BoundsInt bounds = tilemap.cellBounds;
+                TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+                Debug.Log($"bounds {bounds}");
+
+                for (int x = 0; x < bounds.size.x; x++) {
+                    for (int y = 0; y < bounds.size.y; y++) {
+                        TileBase tile = allTiles[x + y * bounds.size.x];
+                        if (tile != null) {
+                            Debug.Log($"x:{x} y:{y} tile:{tile.name} ");
+                        } else {
+                            Debug.Log($"x:{x} y:{y} tile: (null)");
+                        }
+                    }
+                }
+
+                if (tilemap.HasTile(cellPosition)) {
+                    Debug.Log($"Had tile at position {cellPosition}");
+                }
+                tilemap.SetTile(cellPosition, null);
             }
         }
     }
