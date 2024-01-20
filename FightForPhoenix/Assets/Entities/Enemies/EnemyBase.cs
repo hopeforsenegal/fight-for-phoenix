d9f756ad41214712ffc4359114ec825e3610b800
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-20 11:36:13 -0600

    moved enemy baseline components to config with basic sine movement.

--- FightForPhoenix/Assets/Entities/Enemies/EnemyBase.cs
+++ FightForPhoenix/Assets/Entities/Enemies/EnemyBase.cs
@@ -1,20 +1,8 @@
 using UnityEngine;
 
-public class EnemyBase : using UnityEngine;
-
 [CreateAssetMenu(fileName = "EnemyBase", menuName = "FightForPhoenix/EnemyBase", order = 0)]
 public class EnemyBase : ScriptableObject {
-    public float Speed {set;get;}
-
+    //entity attempts to dodge or not
     
-
-    void LookAtPlanet() {
-        var offset = 90f;
-        Vector2 facingDirection = Target.position - transform.position;
-        facingDirection.Normalize();
-        moveDirection = new Vector3(facingDirection.x, facingDirection.y, 0f);
-        float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;       
-        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
-    }
 }
 
