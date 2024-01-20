c4b7453f884f4b989f4e7448411c5074948b050e
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-20 17:07:42 -0600

    Player shotgun attack

--- FightForPhoenix/Assets/Entities/Player.cs
+++ FightForPhoenix/Assets/Entities/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     public TrailRenderer TrailRenderer { get; private set; }
     public Transform BulletSpawn;
+    public GameObject laser;
 
     void Awake()
     {
