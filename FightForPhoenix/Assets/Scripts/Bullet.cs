3bb1b54759d07ccebcff19304d2a48389595b0bf
Kamau Vassall <kamau.vassall@gmail.com>
2024-01-19 23:56:51 -0600

    Uh this works now?

--- FightForPhoenix/Assets/Scripts/Bullet.cs
+++ FightForPhoenix/Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine.Tilemaps;
 public class Bullet : MonoBehaviour
 {
     // This will break once there are multple tile maps
+    public GameObject tilemapGameObject;
     Tilemap tilemap;
 
     void Start()
@@ -25,63 +26,14 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        // Convert the hit position to a cell position
-        Vector3 hitPosition3 = other.transform.position;
-        Vector3Int cellPosition3 = tilemap.WorldToCell(hitPosition3);
-
-        // Get the tile at the cell position
-        TileBase tile = tilemap.GetTile(cellPosition3);
-        if (tile != null) {
-            Debug.Log("Hit tile: " + tile.name);
-        }
-
-        if (tilemap.gameObject == other.gameObject) {
-            Debug.Log("Hit tile");
-
-            Vector3 hitPosition = other.transform.position;
-            Vector3Int cellPosition2 = tilemap.WorldToCell(hitPosition);
-            Debug.Log($"cell2 {cellPosition2}");
-
-            //foreach (var hit in other.contacts) {
-            { 
-                Vector3 hitWorldPosition = Vector3.zero;
-               // hitWorldPosition.x = hit.point.x + 0.01f * hit.normal.x;
-               // hitWorldPosition.y = hit.point.y + 0.01f * hit.normal.y;
-                Debug.Log($"hit {hitWorldPosition}");
-
-                Vector3Int cellPosition = tilemap.WorldToCell(hitWorldPosition - new Vector3Int(0, 1, 0));
-                Debug.Log($"cell {cellPosition}");
-
-                BoundsInt bounds = tilemap.cellBounds;
-                TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
-
-                Debug.Log($"bounds {bounds}");
-
-
-                for (int x = tilemap.cellBounds.xMin; x < tilemap.cellBounds.xMax; x++) {
-                    for (int y = tilemap.cellBounds.yMin; y < tilemap.cellBounds.yMax; y++) {
-                        Vector3Int localPlace = new Vector3Int(x, y, (int)tilemap.transform.position.z);
-                        //Vector3 place = tileMap.CellToWorld(localPlace);
-                        //TileBase tile = allTiles[x + y * bounds.size.x];
-                        Debug.Log($"localPlace {localPlace}");
-                        if (cellPosition == localPlace) {
-                            Debug.Log("Hey");
-                        }
-                        //if (tilemap.HasTile(localPlace))
-                        //    if (tile != null) {
-                        //    Debug.Log($"x:{x} y:{y} tile:{tile.name} ");
-                        //} else {
-                        //    Debug.Log($"x:{x} y:{y} tile: (null)");
-                        //}
-                    }
-                }
-
-                if (tilemap.HasTile(cellPosition)) {
-                    Debug.Log($"Had tile at position {cellPosition}");
-                }
-                tilemap.SetTile(cellPosition, null);
+        Vector3 hitPosition = Vector3.zero;
+        if (tilemap != null && tilemapGameObject == collision.gameObject) {
+            foreach (ContactPoint2D hit in collision.contacts) {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
             }
         }
     }
