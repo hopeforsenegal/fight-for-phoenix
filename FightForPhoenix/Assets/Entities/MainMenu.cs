316ce1aac60dace3cc89cc32d2d6bc0fbc933eb0
Kamau Vassall <kamau.vassall@gmail.com>
2024-01-20 20:01:17 -0600

    rudimentary main menu

--- FightForPhoenix/Assets/Entities/MainMenu.cs
+++ FightForPhoenix/Assets/Entities/MainMenu.cs
@@ -1,13 +1,32 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Config config;
+    public Button play;
+    public Button quit;
+    public string nextScene;
 
     protected void Start()
     {
         var a = FindObjectOfType<AudioSource>();
         a.clip = config.Menu;
         a.Play();
+
+        play.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(nextScene);
+        });
+        quit.onClick.AddListener(() =>
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        });
     }
 }
