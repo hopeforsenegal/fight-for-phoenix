51a986968b90a6f418bcef41ba010b627e1eb3d2
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-21 10:39:38 -0600

    has game?

--- FightForPhoenix/Assets/Entities/Enemies/Enemy.cs
+++ FightForPhoenix/Assets/Entities/Enemies/Enemy.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject tilemapGameObject;
+    Tilemap tilemap;
     GameManager _gm;
 
     void Start() {
-        GameManager gm = FindObjectOfType<GameManager>();
-        transform.rotation = gm.config.LookAtTarget(gm.phoenix.transform, gameObject.transform);
+        _gm = FindObjectOfType<GameManager>();
+        transform.rotation = _gm.config.LookAtTarget(_gm.phoenix.transform, gameObject.transform);
+        tilemap = FindObjectOfType<Tilemap>();
+        tilemapGameObject = tilemap.gameObject;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 hitPosition = Vector3.zero;
+        if (tilemap != null && tilemapGameObject == collision.gameObject) {
+            foreach (ContactPoint2D hit in collision.contacts) {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                _gm.OnPlanetCollision(tilemap, tilemap.WorldToCell(hitPosition));
+            }
+        }
     }
 }
