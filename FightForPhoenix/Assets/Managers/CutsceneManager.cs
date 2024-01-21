e6f361256c45c5addace3afa37fdc0401ad733f9
Maria Garcia <79329688+kikidesigns@users.noreply.github.com>
2024-01-21 00:41:31 -0600

    Update CutsceneManager.cs
    added text array and fading logic

--- FightForPhoenix/Assets/Managers/CutsceneManager.cs
+++ FightForPhoenix/Assets/Managers/CutsceneManager.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections;
+using TMPro; // Using TextMeshPro for text elements
 
 public class CutsceneManager : MonoBehaviour
 {
     public Image[] images; // Array of images for the cutscene
+    public TMP_Text[] texts; // Array of texts for the cutscene
     public float timePerImage = 1f; // Time in seconds for each image
     public string gameSceneName = "GameScene"; // Name of the game scene to load
-    public Config config;
+    // public Config config;
 
     private int currentIndex = 0;
     private float timer;
@@ -17,84 +18,84 @@ public class CutsceneManager : MonoBehaviour
 
     void Start()
     {
-        if(images.Length > 0)
+        if (images.Length > 0 && texts.Length == images.Length)
         {
-            DisplayImage(images[currentIndex]);
-        }else{
-            Debug.Log("No images");
+            DisplayImage(images[currentIndex], texts[currentIndex]);
+        }
+        else
+        {
+            Debug.Log("Mismatch in number of images and texts or no images/texts provided");
         }
         timer = timePerImage;
-
-        //for (int i1 = 0; i1 < images.Length; i1++)
-        //{
-        //    var image = images[i1];
-        //    image.sprite = config.sprites[i1];
-        //}
     }
 
     void Update()
     {
         if (currentIndex < images.Length)
         {
-            var current = images[currentIndex];
-            // Fade out current image
+            var currentImage = images[currentIndex];
+            var currentText = texts[currentIndex];
+
+            // Fade out current image and text
             t1 += Time.deltaTime / timePerImage;
             if (t1 > timePerImage)
                 t1 = 1;
-            {
-                current.color = new Color(1f, 1f, 1f, 1f - t1);
-            }
+
+            currentImage.color = new Color(1f, 1f, 1f, 1f - t1);
+            currentText.color = new Color(currentText.color.r, currentText.color.g, currentText.color.b, 1f - t1);
         }
 
         if (currentIndex < images.Length - 1)
         {
-            var next = images[currentIndex + 1];
+            var nextImage = images[currentIndex + 1];
+            var nextText = texts[currentIndex + 1];
 
-            // Fade in next image
+            // Fade in next image and text
             t2 -= Time.deltaTime / timePerImage;
             if (t2 < 0)
                 t2 = 0;
-            {
-                next.color = new Color(1f, 1f, 1f, t2);
-            }
+
+            nextImage.color = new Color(1f, 1f, 1f, t2);
+            nextText.color = new Color(nextText.color.r, nextText.color.g, nextText.color.b, t2);
         }
 
         if (Input.anyKeyDown || (timer -= Time.deltaTime) <= 0)
         {
-            Debug.Log("Do Next");
             if (currentIndex < images.Length)
             {
-                var c = currentIndex;
-                Debug.Log($"Do currentIndex {c}");
-
-                if (c < images.Length - 1)
+                if (currentIndex < images.Length - 1)
                 {
-                    var next = images[c + 1];
-                    // Change image
-                    DisplayImage(next);
+                    var nextImage = images[currentIndex + 1];
+                    var nextText = texts[currentIndex + 1];
+
+                    // Change image and text
+                    DisplayImage(nextImage, nextText);
 
                     t1 = 0;
                     t2 = 1;
                 }
             }
 
-            if(currentIndex == images.Length - 1)
+            if (currentIndex == images.Length - 1)
             {
-                // All images shown, start the game
-                Debug.Log("Starting the game.");
+                // All images and texts shown, start the game
                 SceneManager.LoadScene(gameSceneName);
             }
             timer = timePerImage;
-
             currentIndex += 1;
         }
     }
 
-    void DisplayImage(Image image)
+    void DisplayImage(Image image, TMP_Text text)
     {
-        foreach(var img in images)
+        foreach (var img in images)
         {
             img.gameObject.SetActive(img == image);
+        }
+
+        foreach (var txt in texts)
+        {
+            txt.gameObject.SetActive(txt == text);
         }
     }
 }
